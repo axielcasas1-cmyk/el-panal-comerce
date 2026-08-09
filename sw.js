2b52be25f9d8b46fc3e70cd78e57131ce9c28845
AXUSS <305831321+axielcasas1-cmyk@users.noreply.github.com>
2026-08-09 23:32:42 +0200

    feat: add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='bolsa-ideas-rc1-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.png','./apple-touch-icon-v5.png','./icon192-opt2.png','./icon512-opt2.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
