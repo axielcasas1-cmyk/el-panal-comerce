0c4acdb1fc165a0a316f51ef76873d419e07bf30
AXUSS <305831321+axielcasas1-cmyk@users.noreply.github.com>
2026-08-22 16:48:47 +0200

    Add Dragolaida V10 offline service worker

---
+++ dragolaida-v10/sw.js
@@ -0,0 +1,6 @@
+const CACHE='dragolaida-v10g-shell';
+const ASSETS=['./','./index.html','./core.js','./app.js','./manifest.webmanifest','./icon.svg','./dragolaida-img.js','./bemba-img.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{if(r&&r.ok&&new URL(e.request.url).origin===location.origin){const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));}return r;}).catch(()=>cached)));});
+self.addEventListener('message',e=>{if(e.data==='SKIP_WAITING')self.skipWaiting();});
